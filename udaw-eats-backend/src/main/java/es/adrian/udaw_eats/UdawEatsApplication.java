a4b53da5e188ec3bcd917759829496c1f671a293
Adrián López Espiñeira <lopezesp.adrian@gmail.com>
2025-03-15 17:55:21 +0100

    Configuracion inicial

---
+++ udaw-eats-backend/src/main/java/es/adrian/udaw_eats/UdawEatsApplication.java
@@ -0,0 +1,13 @@
+package es.adrian.udaw_eats;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UdawEatsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UdawEatsApplication.class, args);
+	}
+
+}
---
+++ udaw-eats-backend/src/test/java/es/adrian/udaw_eats/UdawEatsApplicationTests.java
@@ -0,0 +1,13 @@
+package es.adrian.udaw_eats;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UdawEatsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
